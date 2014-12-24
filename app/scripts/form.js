fbe56a810df442174fe6f88f8b2e453c862c5884
Andrew El-Masry <andrewelmasry@gmail.com>
2014-12-23 20:50:38 -0500

    comments

---
+++ app/scripts/form.js
@@ -0,0 +1,12 @@
+(function(d){
+  var l = d.getElementById("foxyform_embed_link_621782");
+  i = d.createElement('IFRAME');
+  i.src = "http://www.foxyform.com/form.php?id=621782&sec_hash=66633b2ab1f";
+  i.width = "350px";
+  i.height = "427px";
+  i.frameborder = 0;
+  i.setAttribute('style', 'border: none;');
+  i.setAttribute('frameborder', '0');
+  l.setAttribute('style', 'display: block; width: 1000px; font-size: 0.7em; color: black; text-align: right; text-decoration: none;');
+  l.parentNode.insertBefore(i,l);
+})(document);

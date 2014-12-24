688cab9fef868f705c0caa56a539cbe82220652e
Andrew El-Masry <andrewelmasry@gmail.com>
2014-12-24 16:57:29 -0500

    commit

--- app/scripts/contact.js
+++ app/scripts/contact.js
@@ -10,7 +10,6 @@
     events: {
         'click .submit'     :   'submit',
         'click .clear'      :   'clear',
-        'click .contact'    :   'reinitialize'
     },
 
     initialize   : function () {
@@ -27,31 +26,93 @@
 
     submit    : function (e) {
       e.preventDefault();
-      $( '.contactForm' ).fadeOut(500);
-      $( '.messSent').delay(500);
-      $( ".messSent" ).fadeIn( 500);
+
+      if($('.nameVal').val() === ""){
+        $('.label1').empty();
+        $('.label1').html('Please put a name').css("color","red");
+
+        $('.label2').empty();
+        $('.label2').html('Your E-mail:').css("color","black");
+        $('.label3').empty();
+        $('.label3').html('Subject:').css("color","black");
+        $('.label4').empty();
+        $('.label4').html('Message:').css("color","black");
+
+        $('.errorSubmit').empty();
+        $('.errorSubmit').html('You are missing a name').css("color","red");
+        console.log('working');
+      }
+      else if($('.emailVal').val() === ""){
+        $('.label2').empty();
+        $('.label2').html('Please put a valid e-mail').css("color","red");
+
+        $('.label1').empty();
+        $('.label1').html('You Name').css("color","black");
+        $('.label3').empty();
+        $('.label3').html('Subject:').css("color","black");
+        $('.label4').empty();
+        $('.label4').html('Message:').css("color","black");
+
+        $('.errorSubmit').empty();
+        $('.errorSubmit').html('You are missing a valid e-mail').css("color","red");
+        console.log('working');
+      }
+      else if($('.subVal').val() === ""){
+        $('.label3').empty();
+        $('.label3').html('Please put a subject').css("color","red");
+
+        $('.label1').empty();
+        $('.label1').html('You Name').css("color","black");
+        $('.label2').empty();
+        $('.label2').html('Your E-mail:').css("color","black");
+        $('.label4').empty();
+        $('.label4').html('Message:').css("color","black");
+
+        $('.errorSubmit').empty();
+        $('.errorSubmit').html('You are missing a subject').css("color","red");
+      }
+
+      else if($('.messVal').val() === ""){
+        $('.label4').empty();
+        $('.label4').html('Please put a message').css("color","red");
 
 
-      // var message = new App.Models.Message({
-      //   name: $('.nameVal').val(),
-      //   email: $('.emailVal').val(),
-      //   subject: $('.subVal').val(),
-      //   message:$('.messVal').val(),
-      // });
-      //
-      // message.save(null, {
-      //   success:function() {
-      //     console.log("Success");
-      //     //Alerts are lame - but quick and easy
-      //     $( ".contactForm" ).fadeOut( "slow", function() {
-      //       // Animation complete.
-      //     });
-      //
-      //   },
-      //   error:function(e) {
-      //     console.dir(e);
-      //   }
-      // });
+        $('.label1').empty();
+        $('.label1').html('You Name').css("color","black");
+        $('.label2').empty();
+        $('.label2').html('Your E-mail:').css("color","black");
+        $('.label3').empty();
+        $('.label3').html('Message:').css("color","black");
+
+        $('.errorSubmit').empty();
+        $('.errorSubmit').html('You are missing a message').css("color","red");
+      }
+
+      else{
+
+
+      var message = new App.Models.Message({
+        name: $('.nameVal').val(),
+        email: $('.emailVal').val(),
+        subject: $('.subVal').val(),
+        message:$('.messVal').val(),
+      });
+
+      message.save(null, {
+        success:function() {
+          console.log("Success");
+          $( '.contactForm' ).fadeOut(500);
+          $( '.messSent').delay(500);
+          $( ".messSent" ).fadeIn( 500);
+        },
+
+        error:function(e) {
+            alert('There was an error in sending the message');
+        }
+
+      });
+
+      }/*end of else*/
 
     },
 
@@ -61,12 +122,6 @@
       $('.emailVal').val('');
       $('.subVal').val('');
       $('.messVal').val('');
-
-    },
-
-    reinitialize    : function (e) {
-      e.preventDefault();
-      location.reload();
 
     }
 

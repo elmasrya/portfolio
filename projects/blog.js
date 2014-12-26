95bf7f73b136cdca71a53c8a74e8b34a7cde0f97
Andrew El-Masry <andrewelmasry@gmail.com>
2014-12-26 16:07:44 -0500

    slider

--- projects/blog.js
+++ projects/blog.js
@@ -14,12 +14,15 @@
     initialize   : function () {
       this.render();
 
+
     },
 
     render  : function () {
       this.$el.html(this.template);
 
     }
+
+
 
   });
 
--- projects/book.js
+++ projects/book.js
@@ -1,5 +1,5 @@
 (function(){
-
+  var count =1;
   App.Views.Book = Parse.View.extend({
 
     el                : '#middle',
@@ -8,17 +8,44 @@
     template        : _.template($('#bookTemp').html()),
 
     events: {
-
+        'click .left' : 'left',
+        'click .right': 'right',
     },
 
     initialize   : function () {
       this.render();
+      $('.left').css("display","none");
+      console.log(count);
 
     },
 
     render  : function () {
       this.$el.html(this.template);
 
+    },
+
+    left : function () {
+      count--;
+      console.log(count);
+        $('.slides').animate({'margin-left':'+=720'}, 1000);
+        if (count===1) {
+          $('.left').css("display","none");
+        };
+        if (count === 5) {
+          $('.right').css("display","inline");
+        }
+    },
+
+    right : function () {
+      count++;
+      console.log(count);
+      if (count > 1){
+        $('.left').css("display","inline");
+        $('.slides').animate({'margin-left':'-=720'}, 1000);
+      };
+      if (count===6) {
+        $('.right').css("display","none")
+      };
     }
 
   });

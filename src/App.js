e94cbc617a46776af77e5cb04bed178e2853b24a
Andrew El-Masry <andrew.r.el-masry.ctr@mail.mil>
2025-02-08 22:13:58 -0500

    added avatars

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import hello from './images/hello.png';
+import family from './images/family.png';
+import hobbies from './images/hobbies.png';
+import career from './images/career.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="home-container">
+            <div className="avatar-container">
+                <div className="image-container career-container">
+                    <span className="image-label">Career</span>
+                    <img src={career} className="me-select career-img" alt="career" />
+                </div>
+                <div className="image-container hello-container">
+                    <span className="image-label hello-label">Hello</span>
+                    <img src={hello} className="me-select hello-img" alt="hello" />
+                </div>
+                <div className="image-container hobbies-container">
+                    <span className="image-label hobbies-label">Hobbies</span>
+                    <img src={hobbies} className="me-select hobbies-img" alt="hobbies" />
+                </div>
+                <div className="image-container family-container">
+                    <span className="image-label">Family</span>
+                    <img src={family} className="me-select family-img" alt="family" />
+                </div>
+            </div>
+        </div>
     </div>
   );
 }
